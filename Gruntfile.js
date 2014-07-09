789f2a16f06f5cac5404b80443122ae13e77ea65
Adriaan Putter <adriaan.putter@eoh.co.za>
2014-07-09 14:42:45 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+module.exports = function (grunt) {
+
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+        // The actual grunt server settings
+        connect: {
+            options: {
+                port: 9000,
+                open: true,
+                // Change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            server: {
+                options: {
+                    keepalive: true
+                }
+            }
+        }
+    });
+
+
+    grunt.registerTask('serve', function (target) {
+        grunt.task.run([
+            'connect:server'
+        ]);
+    });
+
+};
---
+++ preview.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+
+angular.module('filePreviewer', [])
+.directive('openFileDialog', [function () {
+    return {
+        scope: {
+            fileInput: '@openFileDialog'
+        },
+        link: function (scope, elem, attrs) {
+            elem.on('click', function (e) {
+                document.getElementById(scope.fileInput).click();
+            });
+        }
+    }
+}])
+
+.directive('filePreview', [function () {
+    function link(scope, elem, attrs) {
+        debugger;
+        elem.on('click', function () {
+            elem.find('input')[0].click();
+        });
+
+        elem.find('input').on('change', function () {
+            var file = elem.find('input')[0].files[0];
+
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                scope.$apply(function () {
+                    scope.imgSrc = e.target.result;
+
+                });
+            };
+
+            reader.readAsDataURL(file);
+        });
+    };
+
+    return {
+        templateUrl: 'previewTemplate.html',
+        scope: {},
+        link: link
+    };
+}]);
